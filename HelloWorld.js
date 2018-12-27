31eb090887e2a8b9766b94eea20e4143ec47dff1
Majician13 <44847744+Majician13@users.noreply.github.com>
2018-12-27 16:49:38 -0500

    Add files via upload

--- HelloWorld.js
+++ HelloWorld.js
@@ -1,1 +1,58 @@
 document.write("Hello World");
+
+var age = 42;
+document.write(age);
+
+var x = 2;
+var y = 4;
+var z = x + y;
+document.write(z);
+
+var pizza = 1;
+pizza++;
+document.write(pizza);
+
+if (x == 2 && y == 4) {
+    pizza++;
+    document.write(pizza)
+}
+else {
+    document.write("WRONG")
+}   
+
+
+var array_items = [1, 2, 3, 4, 5];
+document.write(array_items);
+
+var i;
+for (i = 1; i <= 10; i++){
+    document.write("Hello World");
+}
+
+var t = 1;
+do {
+    document.write("I Love JS");
+    t = t + 1
+}
+while (t <= 10);
+
+alert("I'm awesome");
+
+prompt("What's your name?");
+
+confirm("Do you really want to delete this picture permanently?");
+
+function print() {
+    document.write("I know JS");
+}
+
+print();
+
+function add(num1, num2) {
+    var result = num1 + num2;
+    document.write(result);
+}
+
+add(3, 4);
+
+
